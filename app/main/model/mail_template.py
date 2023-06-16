23220236f108f18f235dbffc4acb9d1d29ac790b
zhuleyao <2451875383@qq.com>
2023-06-16 14:12:49 +0800

    new model

--- app/main/model/mail_template.py
+++ app/main/model/mail_template.py
@@ -8,6 +8,7 @@ class Mailtemplate(db.Model):
 
     id = db.Column(db.INTEGER, primary_key=True, autoincrement=True, comment='邮件编号')
     subject = db.Column(db.String(255), nullable=False, comment='邮件主题')
+    type = db.Column(db.String(50), nullable=False, comment='邮件类型')
     content = db.Column(db.String(50000), nullable=False, comment='邮件内容')
     attach_id = db.Column(db.INTEGER, nullable=False, comment='附件id')
     createdbyuid = db.Column(db.INTEGER, nullable=False, comment='创建人编号')
@@ -16,3 +17,5 @@ class Mailtemplate(db.Model):
     modifytime = db.Column(db.DateTime, nullable=False, comment='修改时间')
     comments = db.Column(db.String(5120), comment='备注')
     islocked = db.Column(db.BOOLEAN, nullable=False, comment='是否锁定')
+    lockbyuid = db.Column(db.INTEGER, nullable=False, comment='发起锁定的人编号')
+    locktime = db.Column(db.DateTime, nullable=False, comment='锁定时间')
